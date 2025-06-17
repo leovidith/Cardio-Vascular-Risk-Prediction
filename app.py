9e27f9504a2aee3b88c58f13c5a74c21797b57d7
Vidith <kotasaividith@gmail.com>
2025-06-17 12:01:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import pickle
 import numpy as np
 import warnings
-warnings.filterwarnings('ignore')
+
 import os
 
 app = Flask(__name__)
